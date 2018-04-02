5f646d32cfd4b3cd548deb7c2ff10a792ff906a6
Daniel Widrick <daniel@widrick.net>
2018-04-01 23:44:57 -0400

    url generation

--- zap2it-GuideScrape.py
+++ zap2it-GuideScrape.py
@@ -22,16 +22,36 @@ parameters = {
 data = urllib.urlencode(parameters)
 req = urllib2.Request(url,data)
 response = ""
-#response = urllib2.urlopen(req).read()
+response = urllib2.urlopen(req).read()
 
-#zapVars = json.loads(response)
-
-#zapToken = zapVars["token"]
-#print("Token: " + zapToken)
+zapVars = json.loads(response)
+zapToken = "placeHolder"
+zapToken = zapVars["token"]
+print("Token: " + zapToken)
 
 
 currentTimestamp = time.time()
 halfHourOffset = currentTimestamp % (60 * 30)
 closestTimestamp = currentTimestamp - halfHourOffset
 closestTimestamp = int(closestTimestamp)
-print("time stamp: " + str(closestTimestamp) )
+#print("time stamp: " + str(closestTimestamp) )
+parameters = {
+	'Activity_ID': 1,
+	'FromPage': "TV%20Guide",
+	'AffiliateId': "gapzap",
+	'token': zapToken,
+	'aid': 'gapzap',
+	'lineupId':'DFLTE',
+	'timespan':3,
+	'headendId': 'lineupId',
+	'country': Config.get("prefs","country"), 
+	'device': '-',
+	'postalCode': Config.get("prefs","zipCode"),
+	'isOverride': "true",
+	'time': closestTimestamp,
+	'pref': 'm,p',
+	'userId': '-'
+}
+data = urllib.urlencode(parameters)
+url = "https://tvlistings.zap2it.com/api/grid?" + data
+#print(url)
