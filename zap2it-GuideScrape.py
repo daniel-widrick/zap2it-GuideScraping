61fe9de65821054e3fbe28910159e4a889dab370
root <root@Jellyfin.apt.widrick.net>
2019-02-23 23:42:38 +0000

    Sanitize data for xml:

--- zap2it-GuideScrape.py
+++ zap2it-GuideScrape.py
@@ -7,12 +7,20 @@ import time
 import math
 import cgi
 
+def sanitizeData(data):
+	#https://stackoverflow.com/questions/1091945/what-characters-do-i-need-to-escape-in-xml-documents
+	sData = data.replace('"','&quot;')
+	sData = sData.replace("'",'&apos;')
+	sData = sData.replace('<','&lt;')
+	sData = sData.replace('<','&gt;')
+	sData = sData.replace('&','&amp;')
+	return sData;
 def buildXMLChannel(channel):
 	xml = ""
-	xml = xml + '    <channel id="' +  channel["channelId"] + '">' + "\n"
-	xml = xml + '      <display-name>' + channel["channelNo"] + " " + channel["callSign"] + '</display-name>' + "\n"
-	xml = xml + '      <display-name>' + channel["channelNo"] + '</display-name>' + "\n"
-	xml = xml + '      <display-name>' + channel["callSign"] + '</display-name>' + "\n"
+	xml = xml + '    <channel id="' +  sanitizeData(channel["channelId"]) + '">' + "\n"
+	xml = xml + '      <display-name>' + sanitizeData(channel["channelNo"] + " " + channel["callSign"]) + '</display-name>' + "\n"
+	xml = xml + '      <display-name>' + sanitizeData(channel["channelNo"]) + '</display-name>' + "\n"
+	xml = xml + '      <display-name>' + sanitizeData(channel["callSign"]) + '</display-name>' + "\n"
 	xml = xml + '    </channel>' + "\n"
 	return xml
 
@@ -21,12 +29,45 @@ def buildXMLProgram(event,channelId):
 	#20180408120000 +0000
 	xml = ""
 	xml = xml + '    <programme start="' + buildXMLDate(event["startTime"]) + '" '
-	xml = xml + 'stop="' + buildXMLDate(event["endTime"]) + '" channel="' + channelId + '">' + "\n"
-	xml = xml + '      <title lang="en">' + event["program"]["title"].replace('&','+') + '</title>' + "\n"
+	xml = xml + 'stop="' + buildXMLDate(event["endTime"]) + '" channel="' + sanitizeData(channelId) + '">' + "\n"
+	xml = xml + '      <title lang="en">' + sanitizeData(event["program"]["title"]) + '</title>' + "\n"
+	if event["program"]["episodeTitle"] is not None:
+		xml = xml + '      <sub-title lang="en">' + sanitizeData(event["program"]["episodeTitle"]) + ' </sub-title>' + "\n"
 	if event["program"]["shortDesc"] is None:
 		event["program"]["shortDesc"] = "Unavailable"
 	xml = xml + '      <desc lang="en">' + cgi.escape(event["program"]["shortDesc"]) + '</desc>' + "\n"
-	xml = xml + '      <length units="minutes">' + event["duration"] + '</length>' + "\n"
+	xml = xml + '      <length units="minutes">' + sanitizeData(event["duration"]) + '</length>' + "\n"
+	for category in event["filter"]:
+		xml = xml + '      <category>' + sanitizeData(category.replace('filter-','')) + '</category>' + "\n"
+	if event["thumbnail"] is not None:
+		xml = xml + '  <thumbnail>http://zap2it.tmsimg.com/assets/' + event["thumbnail"] + '.jpg</thumbnail>' + "\n"
+	season = "0"
+	episode = "0"
+	episodeid = ""
+	
+	try:
+	#if "season" in event:
+		if event["program"]["season"] is not None:
+			season = str(event["program"]["season"])
+		if event["program"]["episode"] is not None:
+			episode = str(event["program"]["episode"])
+
+	#if "id" in event:
+		if event["program"]["id"] is not None:
+			episodeid = str(event["program"]["id"])
+	except KeyError:
+		print "no season for:" + event["program"]["title"]
+		
+	#print season + "." + episode
+	if int(season) < 10:
+		season = "0" + str(season)
+	if int(episode) < 10:
+		episode = "0" + str(episode)
+	xml = xml + '<episode-num system="SxxExx">S' + season + "E" + episode + "</episode-num>"
+
+	showid = event["seriesId"].replace('SH','')
+	episodeid = episodeid.replace('EP' + showid,'')
+	xml = xml + '<episode-num system="dd_progid">EP' + sanitizeData(showid + '.' + episodeid) + '</episode-num>'
 	
 	xml = xml + '    </programme>'+"\n"
 	return xml
@@ -71,7 +112,7 @@ currentTimestamp = time.time()
 halfHourOffset = currentTimestamp % (60 * 30)
 closestTimestamp = currentTimestamp - halfHourOffset
 closestTimestamp = int(closestTimestamp)
-endTimestamp = closestTimestamp + (60*60*48)
+endTimestamp = closestTimestamp + (60*60*336)
 channelXML = ""
 programXML = ""
 addChannels = True
@@ -111,7 +152,7 @@ while(closestTimestamp < endTimestamp):
 	addChannels = False
 	closestTimestamp = closestTimestamp + (60*60*3)
 	print "Throttling api calls:...."
-	time.sleep(3)
+	#time.sleep(.25)
 
 
 guideXML = '<?xml version="1.0" encoding="ISO-8859-1"?>' + "\n"
@@ -122,6 +163,8 @@ guideXML = guideXML + channelXML
 guideXML = guideXML + programXML
 
 guideXML = guideXML + "\n" + '</tv>'
+
+
 
 file = open("xmlguide.xmltv","w")
 file.write(guideXML.encode('utf8'))
